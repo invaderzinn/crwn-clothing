5eb6350db9e3b76f9cf9ceb6ee7d4477a1cc2824
Lauren Zinnerman <lauren.zinnerman@gmail.com>
2022-06-07 18:59:30 -0400

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+
 
 
 const Shop = () => {
@@ -11,8 +13,9 @@ const App = () => {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />}>
-        <Route path='shop' element={<Shop />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />}/>
+        <Route path='shop' element={<Shop />}/>
       </Route>     
     </Routes>
   ); 
