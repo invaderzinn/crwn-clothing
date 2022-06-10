9d794466ca64c44ca3f9fb774d15f66940649d8e
Lauren Zinnerman <lauren.zinnerman@gmail.com>
2022-06-10 18:17:15 -0400

    created sign up, form input and button components

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,14 @@
 import { useState } from "react";
 
-import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
+import Button from '../button/button.component';
+
+import { 
+    createAuthUserWithEmailAndPassword, 
+    createUserDocumentFromAuth 
+} from "../../utils/firebase/firebase.utils";
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
     displayName: '',
@@ -11,8 +19,8 @@ const defaultFormFields = {
 
 
 const SignUpForm = () => {
-    const [formFileds, setFormFields] = useState(defaultFormFields);
-    const {displayName, email, password, confirmPassword} = formFileds;
+    const [formFields, setFormFields] = useState(defaultFormFields);
+    const {displayName, email, password, confirmPassword} = formFields;
     
 
     const resetFormFields = () => {
@@ -51,16 +59,17 @@ const SignUpForm = () => {
     const handleChange = (event) => {
         const {name, value} = event.target;
 
-        setFormFields({...formFileds, [name]: value});
+        setFormFields({...formFields, [name]: value});
     };
 
     return (
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className="sign-up-container">
+            <h2>Don't have an account</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
 
-                <label>Display Name</label>
-                <input 
+                <FormInput 
+                    label="Display Name"
                     type="text" 
                     required 
                     onChange={handleChange} 
@@ -68,8 +77,8 @@ const SignUpForm = () => {
                     value={displayName} 
                 />
                 
-                <label>Email</label>
-                <input 
+                <FormInput 
+                    label="Email"
                     type="email" 
                     required 
                     onChange={handleChange} 
@@ -77,8 +86,8 @@ const SignUpForm = () => {
                     value={email} 
                 />
                 
-                <label>Password</label>
-                <input 
+                <FormInput
+                    label="Password"
                     type="password" 
                     required 
                     onChange={handleChange} 
@@ -86,15 +95,15 @@ const SignUpForm = () => {
                     value={password} 
                 />
                 
-                <label>Confirm Password</label>
-                <input 
+                <FormInput 
+                    label="Confirm Password"
                     type="password" 
                     required 
                     onChange={handleChange} 
                     name="confirmPassword" 
                     value={confirmPassword} 
                 />
-                <button type="submit">Sign Up</button>
+                <Button type='submit'>Sign Up</Button>
             </form>
         </div>
     );
